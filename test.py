e4ad6c4fb458a7fba4f1ccf1e9ac8c93164aae59
xfortisfye <52729376+xfortisfye@users.noreply.github.com>
2022-10-12 17:38:00 +0800

    generate set04

--- test.py
+++ test.py
@@ -14,31 +14,9 @@ from time import sleep
 import random
 
 def main():
-    startFlag = True
-
-    while startFlag is True:
-        print("What kind of dataset will you like to generate? \n"
-        + "1) Exp 1: Random 10-Letters Dataset for CAPTCHA\n"
-        + "2) Exp 2: Identical 10-Letters Dataset for CAPTCHA\n"
-        + "3) Exp 3: Controlled dataset for Password\n"
-        + "4) Exp 3: Controlled dataset for Password\n"
-        + "5) Exp 4: \n"
-        + "6) Exp 4: \n"
-        + "7) Exp 4: ")
-        case = input()
-        case = int(case)
-        print(case)
-        if case == 1:
-            print('two')
-
-        elif case == 5:
-            print('five')
-
-        elif case == 7:
-            print('seven')
-
-        else:
-            print("Please select the appropriate case number.")
+    text = 7
+    print(f"hello {text}")
+    #  headerList.append(f"T{NG_COUNT}-I|{_}+{_+1}")
 
 if __name__ == "__main__":
     main()
