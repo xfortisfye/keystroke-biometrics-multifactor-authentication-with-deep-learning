bcf339ae420344a456e5c26641175da8c78cd2a1
xfortisfye <52729376+xfortisfye@users.noreply.github.com>
2025-07-13 02:42:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def main():
 
     while startFlag is True:
         print("What kind of dataset will you like to generate? \n"
-        + "1) Exp 1: Random 10-Characters Dataset for CAPTCHA\n"
-        + "2) Exp 2: Identical 10-Characters Dataset for CAPTCHA\n"
+        + "1) Exp 1: Random Ten-Characters CAPTCHA dataset\n"
+        + "2) Exp 2: Identical Ten-Characters CAPTCHA dataset\n"
         + "3) Exp 3: Five-Letters Password Dataset (Random)\n"
         + "4) Exp 3: Five-Letters Password Dataset (Identical)\n"
         + "5) Exp 4: Eight-Letters Password Dataset (Same password: dEcisi0n)"
